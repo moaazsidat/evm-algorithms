b7275b8d348a7beac38336df83379de5d736677e
William Law <williamlaw.wtl@gmail.com>
2022-10-25 13:13:33 -0400

    add: palindrome number

---
+++ src/PalindromeNum.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+// https://leetcode.com/problems/palindrome-number/
+
+contract PalindromeNum {
+    uint256 num;
+
+    function isPalindrome(uint256 _num) public returns (bool) {}
+}
